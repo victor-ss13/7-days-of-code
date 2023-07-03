d85d069eccae1b0e291178a894fa83451afa637d
unknown <victor.sampaio2003@gmail.com>
2023-07-03 13:50:35 -0300

    criando arquivo dia-3 e corrigindo dia-s

--- dia-2/study-2.js
+++ dia-2/study-2.js
@@ -4,6 +4,8 @@ let linguagem = prompt("Qual linguagem de programação você está aprendendo?"
 
 let mensagem1 = `Olá ${nome}, você tem ${idade} e já está aprendendo ${linguagem}!`;
 
+alert(mensagem1);
+
 let duvida = prompt(`Você gosta de estudar ${linguagem}? Digite 1 para SIM e 2 para NÃO.`);
 
 if (duvida == 1) {alert("Muito bom! Continue estudando e você terá muito sucesso.")}
