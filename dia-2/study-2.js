e9e5346558fef9b8f37ea17de42850abd380c86d
unknown <victor.sampaio2003@gmail.com>
2023-07-03 09:36:49 -0300

    criando e resolvendo arquivo dia 2

---
+++ dia-2/study-2.js
@@ -0,0 +1,10 @@
+let nome = prompt("Qual o seu nome?");
+let idade = prompt("Qual sua idade?");
+let linguagem = prompt("Qual linguagem de programação você está aprendendo?");
+
+let mensagem1 = `Olá ${nome}, você tem ${idade} e já está aprendendo ${linguagem}!`;
+
+let duvida = prompt(`Você gosta de estudar ${linguagem}? Digite 1 para SIM e 2 para NÃO.`);
+
+if (duvida == 1) {alert("Muito bom! Continue estudando e você terá muito sucesso.")}
+else if (duvida == 2) {alert("Ahh que pena... Já tentou aprender outras linguagens?")};
